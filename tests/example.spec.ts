8130b138ce301b00143b018a43515ffb7fafe0e0
Liam <liamcurran95@gmail.com>
2025-03-06 11:26:45 +0000

    fail

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -20,3 +20,10 @@ test("has title - 2", async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
+
+test("has title - fail", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle("fail");
+});
